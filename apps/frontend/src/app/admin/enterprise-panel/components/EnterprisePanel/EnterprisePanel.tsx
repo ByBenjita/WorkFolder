46b4d36893cad2f935a25d8cc1e5b8685cc01d10
ByBenjita <benjamin.morales0401@gmail.com>
2026-06-09 13:20:34 -0400

    Remove RRHH_RECEPTORS import from EnterprisePanel

--- apps/frontend/src/app/admin/enterprise-panel/components/EnterprisePanel/EnterprisePanel.tsx
+++ apps/frontend/src/app/admin/enterprise-panel/components/EnterprisePanel/EnterprisePanel.tsx
@@ -8,7 +8,6 @@ import {
   MOCK_LOGS,
   SECURITY_CHECKS,
   INVOICES,
-  RRHH_RECEPTORS,
 } from './useEnterprisePanel';
 
 import BovedaPanel      from '../../../../components/boveda/BovedaPanel';
